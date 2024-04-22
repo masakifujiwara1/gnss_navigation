db30a816425e016597485466c11c5c7f513485f8
masakifujiwara1 <fmasaki2020@gamil.com>
2024-04-22 15:30:00 +0900

    fix pub and update rate

--- gnss_navigation/gnss_point_publisher.py
+++ gnss_navigation/gnss_point_publisher.py
@@ -1,7 +1,7 @@
 import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import NavSatFix
-from geometry_msgs.msg import PoseStamped, Point
+from geometry_msgs.msg import PointStamped, Point, PointStamped
 # import tf_transformations
 from pyproj import Proj, transform
 import pandas as pd
@@ -24,15 +24,13 @@ class GPSPointPublisher(Node):
         self.latitude = 0
         self.longitude = 0
 
-        self.gnss_point = Point()
-
         # 緯度経度をロボット座標系に変換
         # x, y = self.convert_gps_to_utm(data[1], data[0])
 
         self.subscriber = self.create_subscription(NavSatFix, 'gps/fix', self.listener_callback, 10)
 
-        self.publisher_ = self.create_publisher(Point, 'gnss_point', 10)
-        self.timer = self.create_timer(1, self.publish_path)
+        self.publisher_ = self.create_publisher(PointStamped, 'gnss_point', 10)
+        self.timer = self.create_timer(0.025, self.publish_path)
         # self.publish_path(smooth_path)
 
     def listener_callback(self, data):
@@ -47,9 +45,12 @@ class GPSPointPublisher(Node):
     def publish_path(self):
         x, y = self.convert_gps_to_utm(self.longitude, self.latitude)
         self.Point = Point()
+        self.PointStamped = PointStamped()
+        self.PointStamped.header.frame_id = "map"
         self.Point.x = x - self.base_x
         self.Point.y = y - self.base_y
-        self.publisher_.publish(self.Point)
+        self.PointStamped.point = self.Point
+        self.publisher_.publish(self.PointStamped)
         self.get_logger().info("pulish point")
         
 def main():
